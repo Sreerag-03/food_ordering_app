23c77b9f563dd91572cfa48d1b5cb025a7e95a8a
Sreerag-03 <nairsreerag2@gmail.com>
2025-01-10 10:52:01 +0530

    Implemented global search functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,12 +2,36 @@ import 'package:flutter/material.dart';
 import '../models/food_item.dart';
 import '../widgets/food_item_tile.dart';
 
-class HomeScreen extends StatelessWidget {
-  final List<FoodItem> foodItems = [
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<FoodItem> foodItems = [
     FoodItem(id: '1', name: 'Burger', imageUrl: 'https://via.placeholder.com/150', price: 5.99),
     FoodItem(id: '2', name: 'Pizza', imageUrl: 'https://via.placeholder.com/150', price: 9.99),
     FoodItem(id: '3', name: 'Pasta', imageUrl: 'https://via.placeholder.com/150', price: 7.99),
+    FoodItem(id: '4', name: 'Sandwich', imageUrl: 'https://via.placeholder.com/150', price: 4.99),
   ];
+
+  List<FoodItem> filteredItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredItems = foodItems; // Show all items by default
+  }
+
+  void _searchItems(String query) {
+    final results = foodItems.where((item) {
+      return item.name.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+
+    setState(() {
+      filteredItems = results;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +44,29 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               Navigator.pushNamed(context, '/cart');
             },
-          )
+          ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: _searchItems,
+              decoration: InputDecoration(
+                hintText: 'Search food...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
       body: ListView.builder(
-        itemCount: foodItems.length,
+        itemCount: filteredItems.length,
         itemBuilder: (context, index) {
-          return FoodItemTile(item: foodItems[index]);
+          return FoodItemTile(item: filteredItems[index]);
         },
       ),
     );
